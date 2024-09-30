d24cb83c6a0a54719a94bb011a7b6d18164a8215
inerxia <erxinxiao@gmail.com>
2024-09-30 22:11:05 +0700

    edit: docker password

--- build-docker-etalert-backend.sh
+++ build-docker-etalert-backend.sh
@@ -1,4 +1,4 @@
 docker build -t docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA -f ./Dockerfile .
 docker tag docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA docker.io/inerxia/etalert-backend:latest
-docker login -u inerxia -p $CI_REGISTRY_PASSWORD
+docker login -u inerxia --password-stdin $CI_REGISTRY_PASSWORD
 docker image push --all-tags docker.io/inerxia/etalert-backend
