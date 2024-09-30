305506de2a7cfbbe3614e9f91ff3ba28bbde7a41
inerxia <erxinxiao@gmail.com>
2024-09-30 22:03:15 +0700

    edit: ci docker user

--- build-docker-etalert-backend.sh
+++ build-docker-etalert-backend.sh
@@ -1,4 +1,4 @@
-docker build -t etalert-backend:$CI_COMMIT_SHORT_SHA -f  ./Dockerfile .
-docker tag etalert-backend:$CI_COMMIT_SHORT_SHA etalert-backend:latest
+docker build -t docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA -f ./Dockerfile .
+docker tag docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA docker.io/inerxia/etalert-backend:latest
 docker login -u inerxia -p $CI_REGISTRY_PASSWORD
-docker image push --all-tags etalert-backend
+docker image push --all-tags docker.io/inerxia/etalert-backend
