da1aa4de26b073707cfa7d21175c4a48fe86d6ea
inerxia <erxinxiao@gmail.com>
2024-10-02 21:58:47 +0700

    add:build or amd64

--- build-docker-etalert-backend.sh
+++ build-docker-etalert-backend.sh
@@ -1,4 +1,4 @@
-docker build -t docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA -f ./Dockerfile .
+docker buildx build --platorm=linux/amd64,linux/arm64 -t docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA -f ./Dockerfile .
 docker tag docker.io/inerxia/etalert-backend:$CI_COMMIT_SHORT_SHA docker.io/inerxia/etalert-backend:latest
 echo $CI_REGISTRY_PASSWORD | docker login -u "inerxia" --password-stdin
 docker image push --all-tags docker.io/inerxia/etalert-backend
